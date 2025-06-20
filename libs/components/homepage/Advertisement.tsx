4615bdfb75ea1ed5cf4581f17be934d82bf6d18c
SirojAli <siroj.origin.profile@gmail.com>
2025-06-21 07:32:41 +0900

    feat: Develop Cars Ads in Homepage

--- libs/components/homepage/Advertisement.tsx
+++ libs/components/homepage/Advertisement.tsx
@@ -5,9 +5,7 @@ import { Stack } from '@mui/material';
 const Advertisement = () => {
 	return (
 		<Stack className={'video-frame'}>
-			<Stack className={'container'}>VIDEO LIST</Stack>
-
-			{/* <video
+			<video
 				autoPlay
 				muted
 				loop
@@ -15,8 +13,8 @@ const Advertisement = () => {
 				preload="auto"
 				style={{ width: '100%', height: '100%', objectFit: 'cover' }}
 			>
-				<source src="/video/ads.mov" type="video/mp4" />
-			</video> */}
+				<source src="/video/ads.mp4" type="video/mp4" />
+			</video>
 		</Stack>
 	);
 };
