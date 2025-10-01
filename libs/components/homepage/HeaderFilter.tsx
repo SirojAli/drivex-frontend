f2df64ca5e1832c63619ce471b761a122981a806
SirojAli <siroj.origin.profile@gmail.com>
2025-10-01 22:08:54 +0900

    Fix: rename brand/type images to uppercase 3

--- libs/components/homepage/HeaderFilter.tsx
+++ libs/components/homepage/HeaderFilter.tsx
@@ -225,7 +225,7 @@ const HeaderFilter = (props: HeaderFilterProps) => {
 							{carType.map((type: string) => {
 								return (
 									<div
-										style={{ backgroundImage: `url(/img/banner/brands/${type}.png)` }}
+										style={{ backgroundImage: `url(/img/banner/brands/${type}.jpg)` }}
 										onClick={() => carTypeSelectHandler(type)}
 										key={type}
 									>
